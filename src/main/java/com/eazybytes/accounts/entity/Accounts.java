b44fe260e4c0eea2d201866319601017adebaa05
MOHIT KUMAR SHAW <mohitshawmath@gmail.com>
2025-02-11 01:30:26 +0530

    entity

--- src/main/java/com/eazybytes/accounts/entity/Accounts.java
+++ src/main/java/com/eazybytes/accounts/entity/Accounts.java
@@ -1,4 +1,24 @@
 package com.eazybytes.accounts.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Accounts {
+    @Column(name = "customer_id")
+    private long customerId;
+    @Id
+    @Column(name = "account_number")
+    private long accountNumber;
+    @Column(name = "account_type")
+    private String accountType;
+    @Column(name = "branch_address")
+    private String branchAddress;
 }
--- src/main/java/com/eazybytes/accounts/entity/Customer.java
+++ src/main/java/com/eazybytes/accounts/entity/Customer.java
@@ -1,4 +1,27 @@
 package com.eazybytes.accounts.entity;
 
-public class Customer {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Customer extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "customer_id")
+    private long customerId;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "mobile_number")
+    private String mobileNumber;
 }
