ba83ab55a977f416480fbaee869666c244737590
MOHIT KUMAR SHAW <mohitshawmath@gmail.com>
2025-02-11 09:20:48 +0530

    "Added Dto and Repository classes"

---
+++ src/main/java/com/eazybytes/accounts/dto/AccountDto.java
@@ -0,0 +1,10 @@
+package com.eazybytes.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountDto {
+    private long accountNumber;
+    private String accountType;
+    private String branchAddress;
+}
